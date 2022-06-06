86a491dd515cb22fcebbf74aec7d449b6b6db0a9
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-06 03:58:58 +0300

    not done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+
 // Write your solution in this file!
+var customerName = 'bob' ;
+function upperCaseCustomerName(){
+    let customerName ='bob';
+    return customerName.toUpperCase()
+}
+upperCaseCustomerName();
+
+
+function setBestCustomer(){
+ return bestCustomer
+}
+setBestCustomer();
+
+let bestCustomer = 'not bob';
+function overwriteBestCustomer(){
+    const bestCustomer ='maybe bob'
+return bestCustomer
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer= 'Assignment to constant variable.'
+function changeLeastFavoriteCustomer(){
+    var leastFavoriteCustomer ='hello'
+    return leastFavoriteCustomer
+}
+changeLeastFavoriteCustomer();
